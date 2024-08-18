a7c280bf4a183c9145f09beea2bf362bd26c7e15
Ignaamunoz24 <ignaamunoz24@gmail.com>
2024-08-18 12:21:26 -0300

    Testeo Personaje Básico
    *Script movimiento TopDown Personaje
    *Animación Idle
    *Testeo animación del risco
    *Grilla creada para futura creación del mapa

---
+++ Assets/Scripts/Player/TopDownMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        moveInput.x = Input.GetAxis("Horizontal");
+        moveInput.y = Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveInput * moveSpeed * Time.fixedDeltaTime);
+    }
+}
+
